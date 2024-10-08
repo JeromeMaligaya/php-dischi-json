ac720a61afeca439176d1b4426a4f0881582a5d9
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-08 16:46:58 +0200

    imported axios, vue, css and js on index.php

--- index.php
+++ index.php
@@ -11,24 +11,39 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+    <!-- vue -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+    <!-- axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.7/axios.min.js"
+        integrity="sha512-DdX/YwF5e41Ok+AI81HI8f5/5UsoxCVT9GKYZRIzpLxb8Twz4ZwPPX+jQMwMhNQ9b5+zDEefc+dcvQoPWGNZ3g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <!-- css -->
+    <link href="css/stylecss" type="text/css">
+
+    <!-- script.js -->
+    <script defer src="script/script.js"></script>
+
 </head>
 
 <body>
-
-    <main>
-        <div class="container mt-4">
-            <div class="row">
-                <div class="col-12">
-                    <h1 class="fw-bolder text-center py-3">php-dischi-json</h1>
+    <div id="app">
+        <main>
+            <div class="container mt-4">
+                <div class="row">
+                    <div class="col-12">
+                        <h1 class="fw-bolder text-center py-3">php-dischi-json</h1>
+                    </div>
                 </div>
             </div>
-        </div>
-    </main>
-
+        </main>
+    </div>
     <!-- js bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
+
 </body>
 
 </html>
