3b39a21201761b7b1d3414a9119cfc136dee10fa
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-08 22:14:13 +0200

    added getDiskList(), apiUrl and listDisks[]

--- script/script.js
+++ script/script.js
@@ -3,9 +3,31 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!',
+            apiUrl: 'http://localhost/php-dischi-json/api/api.php',
+            listDisks: [],
         }
     },
     methods: {
+        getDiskList() {
+            axios.get(this.apiUrl)
+
+                .then(response => {
+                    // handle success
+                    console.log('response', response);
+                    this.listDisks = response.data.data;
+                    console.log('listDisks', this.listDisks)
+
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+                .finally(function () {
+                    // always executed
+                });
+        },
+    },
+    created() {
+        this.getDiskList()
     }
 }).mount('#app')
